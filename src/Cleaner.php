b1e17b32d595e380d32fb53f31645dc0c5775696
Gildas de Cadoudal <gildas.decadoudal@gsoi.fr>
2021-06-04 17:23:14 +0200

    fix: sync with upstream

--- src/Cleaner.php
+++ src/Cleaner.php
@@ -144,11 +144,6 @@ class Cleaner
             }
 
             if ($attempts > $this->task['attempts'] || !$this->engine->removeFailureJob($offset)) {
-                // Handle mail alert
-                if (isset($alert) && $alert) {
-                    $this->engine->sendMail('failure', $result);
-                }
-
                 $items = $this->next($offset++);
                 continue;
             }
